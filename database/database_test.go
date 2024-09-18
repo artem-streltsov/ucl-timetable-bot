3ebcf403bead9aa65e60350806fef02a2cd3f323
Artem Streltsov <artem@streltsov.dev>
2024-09-18 23:43:21 +0200

    add database tests

--- database/database_test.go
+++ database/database_test.go
@@ -60,16 +60,45 @@ func TestGetLastSentTimes(t *testing.T) {
 	err = database.UpdateLastDailySent(db, chatID, now)
 	assert.NoError(t, err)
 
-	err = database.UpdateLastWeeklySent(db, chatID, now)
-	assert.NoError(t, err)
-
 	lastDailySent, err := database.GetLastDailySentTime(db, chatID)
 	assert.NoError(t, err)
 	assert.WithinDuration(t, now, lastDailySent, time.Second)
 
+	err = database.UpdateLastWeeklySent(db, chatID, now)
+	assert.NoError(t, err)
+
 	lastWeeklySent, err := database.GetLastWeeklySentTime(db, chatID)
 	assert.NoError(t, err)
 	assert.WithinDuration(t, now, lastWeeklySent, time.Second)
+}
+
+func TestGetUserPreferences(t *testing.T) {
+	defer os.Remove(testDBPath)
+
+	db, err := database.InitDB(testDBPath)
+	assert.NoError(t, err)
+	defer db.Close()
+
+	chatID := int64(123456)
+	webcalURL := "https://example.com/calendar"
+
+	err = database.InsertUser(db, chatID, webcalURL)
+	assert.NoError(t, err)
+
+	dailyTime, weeklyTime, offset, err := database.GetUserPreferences(db, chatID)
+	assert.NoError(t, err)
+	assert.Equal(t, "18:00", dailyTime)
+	assert.Equal(t, "SUN 18:00", weeklyTime)
+	assert.Equal(t, 30, offset)
+
+	err = database.UpdateUserPreferences(db, chatID, "08:00", "SAT 12:00", 15)
+	assert.NoError(t, err)
+
+	dailyTime, weeklyTime, offset, err = database.GetUserPreferences(db, chatID)
+	assert.NoError(t, err)
+	assert.Equal(t, "08:00", dailyTime)
+	assert.Equal(t, "SAT 12:00", weeklyTime)
+	assert.Equal(t, 15, offset)
 }
 
 func TestGetAllUsers(t *testing.T) {
@@ -100,4 +129,71 @@ func TestGetAllUsers(t *testing.T) {
 		assert.Equal(t, users[i].chatID, user.ChatID)
 		assert.Equal(t, users[i].webcalURL, user.WebcalURL)
 	}
+}
+
+func TestUpdateLastDailySent(t *testing.T) {
+	defer os.Remove(testDBPath)
+
+	db, err := database.InitDB(testDBPath)
+	assert.NoError(t, err)
+	defer db.Close()
+
+	chatID := int64(123456)
+	webcalURL := "https://example.com/calendar"
+	err = database.InsertUser(db, chatID, webcalURL)
+	assert.NoError(t, err)
+
+	now := time.Now()
+
+	err = database.UpdateLastDailySent(db, chatID, now)
+	assert.NoError(t, err)
+
+	lastDailySent, err := database.GetLastDailySentTime(db, chatID)
+	assert.NoError(t, err)
+	assert.WithinDuration(t, now, lastDailySent, time.Second)
+}
+
+func TestUpdateLastWeeklySent(t *testing.T) {
+	defer os.Remove(testDBPath)
+
+	db, err := database.InitDB(testDBPath)
+	assert.NoError(t, err)
+	defer db.Close()
+
+	chatID := int64(123456)
+	webcalURL := "https://example.com/calendar"
+	err = database.InsertUser(db, chatID, webcalURL)
+	assert.NoError(t, err)
+
+	now := time.Now()
+
+	err = database.UpdateLastWeeklySent(db, chatID, now)
+	assert.NoError(t, err)
+
+	lastWeeklySent, err := database.GetLastWeeklySentTime(db, chatID)
+	assert.NoError(t, err)
+	assert.WithinDuration(t, now, lastWeeklySent, time.Second)
+}
+
+func TestUpdateUserPreferences(t *testing.T) {
+	defer os.Remove(testDBPath)
+
+	db, err := database.InitDB(testDBPath)
+	assert.NoError(t, err)
+	defer db.Close()
+
+	chatID := int64(123456)
+	webcalURL := "https://example.com/calendar"
+
+	err = database.InsertUser(db, chatID, webcalURL)
+	assert.NoError(t, err)
+
+	err = database.UpdateUserPreferences(db, chatID, "07:30", "SAT 09:00", 20)
+	assert.NoError(t, err)
+
+	dailyTime, weeklyTime, offset, err := database.GetUserPreferences(db, chatID)
+	assert.NoError(t, err)
+	assert.Equal(t, "07:30", dailyTime)
+	assert.Equal(t, "SAT 09:00", weeklyTime)
+	assert.Equal(t, 20, offset)
 }
