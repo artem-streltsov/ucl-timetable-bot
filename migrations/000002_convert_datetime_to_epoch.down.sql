d4235de947b63d443030da042e415151ac77dc59
Artem Streltsov <artem@streltsov.dev>
2024-09-22 22:39:32 +0100

    fix migrations

--- migrations/000002_convert_datetime_to_epoch.down.sql
+++ migrations/000002_convert_datetime_to_epoch.down.sql
@@ -1,9 +1,11 @@
 ALTER TABLE users ADD COLUMN lastDailySent_datetime DATETIME;
-UPDATE users SET lastDailySent_datetime = datetime(lastDailySent, 'unixepoch');
-ALTER TABLE users DROP COLUMN lastDailySent;
-ALTER TABLE users RENAME COLUMN lastDailySent_datetime TO lastDailySent;
-
 ALTER TABLE users ADD COLUMN lastWeeklySent_datetime DATETIME;
+
+UPDATE users SET lastDailySent_datetime = datetime(lastDailySent, 'unixepoch');
 UPDATE users SET lastWeeklySent_datetime = datetime(lastWeeklySent, 'unixepoch');
+
+ALTER TABLE users DROP COLUMN lastDailySent;
 ALTER TABLE users DROP COLUMN lastWeeklySent;
+
+ALTER TABLE users RENAME COLUMN lastDailySent_datetime TO lastDailySent;
 ALTER TABLE users RENAME COLUMN lastWeeklySent_datetime TO lastWeeklySent;
